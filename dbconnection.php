7b87b1ea7489a18caafebe2ef3bf2c92f9a33f36
Brendan <bodowd@unca.edu>
2019-04-08 22:05:56 -0400

    DB connect

---
+++ dbconnection.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "cwl.cs.unca.edu";
+$username = "ewarren1";
+$password = "mysql4you";
+
+// Create connection
+$conn = new mysqli_connect($servername, $username, $password, 'ewarren1DBCSCI338');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+
+?>
