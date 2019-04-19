d22ed3e3badf12fdebde55455f592989734215fd
aweiler97 <aweiler@unca.edu>
2019-04-18 20:41:19 -0400

    PHP Format and Product Page

--- dbconnection.php
+++ dbconnection.php
@@ -2,7 +2,7 @@
 
 $servername = "avl.cs.unca.edu";
 $username = "ewarren1";
-$password = "mysql4you";
+$password = "sql4you";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, 'ewarren1DBCSCI338');
