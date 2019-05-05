a1d181608ae2517b2fe1d768ca01253675e57dbc
Brendan O'Dowd <31706197+odowdbrendan@users.noreply.github.com>
2019-05-05 13:56:43 -0400

    Update order_php.php

--- order_php.php
+++ order_php.php
@@ -1,20 +1,25 @@
 <?php
-  session_start();
-    
-    $conn = mysqli_connect("avl.cs.unca.edu", "ewarren1", "sql4you", "ewarren1DBCSCI338") or die("Connection failed: ");
-    
-    
-    $idProduct = $_POST['idProduct'];
-    $productQuantity = $_POST['productQuantity'];
-    //$date = date(Y-m-d); 
-    
-    $sql = "INSERT INTO Orders(idProduct, amountShipped, idAccount)
-             VALUES('$idProduct', '$productQuantity', '5')";
-    
-    if(!mysqli_query($conn, $sql)) {
-        echo "An error occured when inserting the product";
-    } else {
-        echo "Product inserted successfully.";
-    }
-   // mysqli_close($conn);
+$conn = mysqli_connect("avl.cs.unca.edu", "ewarren1", "sql4you", "ewarren1DBCSCI338") or die("Connection failed: ");
+
+$idProduct = $_POST['idProduct'];
+$title = $_POST['title'];
+$first = $_POST['first'];
+$middle = $_POST['middle'];
+$last = $_POST['last'];
+$amountShipped = $_POST['amountShipped'];
+$date = $_POST['date'];
+$accountID = $_POST['accountID'];
+$price = $_POST['priceTotal'];
+
+
+$sql = "INSERT INTO Orders(Product_idProduct, Title, First, Middle, Last, amountShipped, dateOfOrder, Account_idAccount, price_Total)
+             VALUES('$idProduct', '$title', '$first', '$middle', '$last', '$amountShipped', '$date', '$accountID', '$price')";
+
+if(!mysqli_query($conn, $sql)) {
+    echo "An error occured when ordering the product";
+} else {
+    echo "Product inserted successfully.";
+}
+
+mysqli_close($conn);
 ?>
