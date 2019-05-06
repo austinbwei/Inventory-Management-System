d90ffaa2806e38a652c9491647a7507a34ca9ebe
Brendan O'Dowd <31706197+odowdbrendan@users.noreply.github.com>
2019-05-06 12:08:29 -0400

    Update update_account.php

--- update_account.php
+++ update_account.php
@@ -1,7 +1,8 @@
 <?php
-    include('dbconnection.php');
-    
-    $userName = $_POST['userName'];
+$conn = mysqli_connect("avl.cs.unca.edu", "ewarren1", "sql4you", "ewarren1DBCSCI338");
+
+
+    $userName = $_SESSION['username'];
     $email = $_POST['email'];
     $firstName = $_POST['firstName'];
     $middleName = $_POST['middleName'];
@@ -11,16 +12,18 @@
    
     
     $sql = "UPDATE `Account` SET 
-           `userName`='".$userName."',`email`='".$email."',`firstName`= '".$firstName."',
-           `middleName`='".$middleName."',`lastName`='".$lastName."',
-           `companyName`='".$companyName."',`passowrd`= $password
-           WHERE `idProduct`= $idProduct";
-    $result= mysqli_query($conn, $sql);
+           `userName`='".$userName."',`email`='".$email."',`First`= '".$firstName."',
+           `Middle`='".$middleName."',`Last`='".$lastName."',
+           `companyName`='".$companyName."',`password`= $password  WHERE `userName`= {$_SESSION['username']}";
+                 
+ 
+$result= mysqli_query($conn, $sql);
     
-    if($result) {
-        echo 'Product Updated';
-    } else {
-        echo 'Update Failed';
-    }
-    mysqli_close($conn);
+ 
+if($result) {
+    echo 'Account Updated';
+} else {
+    echo 'Account Failed';
+}
+mysqli_close($conn);
 ?>
